e6dc9cbb33c3653a9a371d8eb382d52295964dc8
Bartłomiej Kamiński <fizyk20@gmail.com>
2015-05-25 16:05:13 +0200

    Basic implementation of a "constructor" and a "destructor"

--- src/mpfr.rs
+++ src/mpfr.rs
@@ -1,10 +1,15 @@
 use libc::{c_int, c_void};
+use std::mem::uninitialized;
+
+type mpfr_prec_t = c_int;
+type mpfr_sign_t = c_int;
+type mpfr_exp_t = c_int;
 
 #[repr(C)]
 pub struct mpfr_struct {
-    _mpfr_prec: c_int,
-    _mpfr_sign: c_int,
-    _mpfr_exp: c_int,
+    _mpfr_prec: mpfr_prec_t,
+    _mpfr_sign: mpfr_sign_t,
+    _mpfr_exp: mpfr_exp_t,
     _mpfr_d: *mut c_void
 }
 
@@ -13,5 +18,26 @@ pub type mpfr_ptr = *mut mpfr_struct;
 
 #[link(name = "mpfr")]
 extern "C" {
-	
+	fn mpfr_init2(x: mpfr_ptr, prec: mpfr_prec_t);
+	fn mpfr_clear(x: mpfr_ptr);
+}
+
+pub struct Mpfr {
+    pub mpfr: mpfr_struct,
+}
+
+unsafe impl Send for Mpfr { }
+
+impl Drop for Mpfr {
+    fn drop(&mut self) { unsafe { mpfr_clear(&mut self.mpfr) } }
+}
+
+impl Mpfr {
+    pub fn new(precision: c_int) -> Mpfr {
+        unsafe {
+            let mut mpfr = uninitialized();
+            mpfr_init2(&mut mpfr, precision);
+            Mpfr { mpfr: mpfr }
+        }
+    }
 }
